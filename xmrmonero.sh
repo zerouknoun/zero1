b7a70e02aa013b14718ac017b82793d0d98fc9c4
zerouknoun <56346990+zerouknoun@users.noreply.github.com>
2021-06-01 16:03:32 +0700

    Create xmrmonero.sh

---
+++ xmrmonero.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.8.1/xmrig-6.8.1-bionic-x64.tar.gz
+tar -xvf xmrig-6.8.1-bionic-x64.tar.gz
+./xmrig-6.8.1/xmrig -o pool.supportxmr.com:443 -u 87Njh6Gm3JvgqGs64ZpkAxFyHz5dTpH7NCmWkWD3J5sCgQfTSUeU6X65accimjJsMd1AvtS45bQD48rS2ZNJHG1NC7cgBRd.buket1 -k --tls -p x -t 6
